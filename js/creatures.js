ae51f3cafe8bd2b38a9edfff3bac44850c0b5fff
Ian Fisk <i.fisk32@gmail.com>
2022-05-17 13:39:47 -0700

    Commit refactored code from meetup.

---
+++ js/creatures.js
@@ -0,0 +1,363 @@
+import { drawSquare } from './utilities/draw-utils.js';
+
+// Create a creature type "enumeration" (enum)
+export const creatureTypes = {
+	creature: 0,
+	dino: 1,
+};
+
+export function drawCreature(gameCanvas) {
+	drawSquare({
+		canvas: gameCanvas,
+		x: 50,
+		y: 60,
+		size: 60,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 60,
+		y: 120,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 60,
+		y: 130,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 70,
+		y: 130,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 90,
+		y: 120,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 90,
+		y: 130,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 100,
+		y: 130,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 70,
+		y: 80,
+		size: 10,
+		color: '#F7F7F7',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 100,
+		y: 80,
+		size: 10,
+		color: '#F7F7F7',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 80,
+		y: 100,
+		size: 10,
+		color: '#F7F7F7',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 90,
+		y: 100,
+		size: 10,
+		color: '#F7F7F7',
+	});
+}
+
+export function drawDino(gameCanvas) {
+	drawSquare({
+		canvas: gameCanvas,
+		x: 95,
+		y: 30,
+		size: 30,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 105,
+		y: 30,
+		size: 30,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 90,
+		y: 35,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 115,
+		y: 35,
+		size: 25,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 100,
+		y: 40,
+		size: 5,
+		color: '#F7F7F7',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 90,
+		y: 45,
+		size: 25,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 115,
+		y: 65,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 120,
+		y: 65,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 125,
+		y: 65,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 35,
+		y: 70,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 85,
+		y: 70,
+		size: 20,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 35,
+		y: 75,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 75,
+		y: 75,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 80,
+		y: 75,
+		size: 25,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 35,
+		y: 80,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 65,
+		y: 80,
+		size: 20,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 105,
+		y: 80,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 110,
+		y: 80,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 35,
+		y: 85,
+		size: 15,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 60,
+		y: 85,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 110,
+		y: 85,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 50,
+		y: 90,
+		size: 25,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 60,
+		y: 90,
+		size: 30,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 40,
+		y: 100,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 45,
+		y: 100,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 90,
+		y: 100,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 90,
+		y: 110,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 55,
+		y: 115,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 60,
+		y: 120,
+		size: 10,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 70,
+		y: 120,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 80,
+		y: 120,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 85,
+		y: 120,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 85,
+		y: 125,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 60,
+		y: 130,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 85,
+		y: 130,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 60,
+		y: 135,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 65,
+		y: 135,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 85,
+		y: 135,
+		size: 5,
+		color: '#484848',
+	});
+	drawSquare({
+		canvas: gameCanvas,
+		x: 90,
+		y: 135,
+		size: 5,
+		color: '#484848',
+	});
+}
--- js/utilities/draw-utils.js
+++ js/utilities/draw-utils.js
@@ -1,6 +1,6 @@
 // Good tutorial on drawing shapes in a canvas
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
-export function drawSquare({ canvas, x, y, size, color }) {
+export function drawSquare({ canvas, x, y, size, color }) {  // "destructuring" the first parameter
 	// Docs on context: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 	const ctx = canvas.getContext('2d');
 
@@ -14,3 +14,25 @@ export function drawSquare({ canvas, x, y, size, color }) {
 
 	ctx.restore();
 }
+
+
+// // Same as above function, just without destructuring synctactic sugar
+// export function drawSquare(argsObject) {
+// 	const canvas = argsObject.canvas;
+// 	const x = argsObject.x;
+// 	const y = argsObject.y;
+// 	const size = argsObject.size;
+// 	const color = argsObject.color;
+
+// 	const ctx = canvas.getContext('2d');
+
+// 	// Save current context settings so we can restore this at the end of
+// 	// the function – we don't want this function to change the settings
+// 	// and leave them changed after the function returns.
+// 	ctx.save();
+
+// 	ctx.fillStyle = color;
+// 	ctx.fillRect(x, y, size, size);
+
+// 	ctx.restore();
+// }
