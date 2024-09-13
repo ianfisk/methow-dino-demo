f07914dd56ab5bd63a20caa49960f1c34154e6f1
Ian Fisk <i.fisk32@gmail.com>
2024-09-12 18:00:27 -0600

    Keep creating obstacles.

--- js/index.js
+++ js/index.js
@@ -9,26 +9,24 @@ const gameCanvas = createCanvas({
 	heightPx: 400,
 });
 
-let frameCount = 0;
-let previousFrameTimestamp = 0;
-let leg_motion = 0;
-
-
 // TODO: move these into a gameState object; each of these values
 // would be a property of that object, and the gameState could be
 // referenced where ever you need to know the speed of the dino or
 // another value.
-const dinoRunSpeed = 300; // Pixels per second
-const dinoLegToggleFrameCount = 15; // Number of frames before toggling leg animation
-const creatureType = creatureTypes.dino;
-const obstacleType = obstacleTypes.cactus;
-const startingx = 35;
-const startingy = 230;
-let objStartingx = 605;
-const objStartingy = 230;
+const DINO_RUN_SPEED = 300; // Pixels per second
+const DINO_LEG_TOGGLE_FRAME_COUNT = 15; // Number of frames before toggling leg animation
+const CREATURE_TYPE = creatureTypes.dino;
+const OBSTACLE_TYPE = obstacleTypes.cactus;
+const PLAYER_X = 35; // Screen origin is the upper-left corner.
+const PLAYER_Y = 230;
+const OBSTACLE_Y = 230;
+const INITIAL_OBSTACLE_X = 1000;
+const MIN_OBSTACLE_SPACING = 200;
 
 let dinoJumpStartTime = -1;
-
+let frameCount = 0;
+let previousFrameTimestamp = 0;
+let obstacleXCoords = getRandomObstacles(INITIAL_OBSTACLE_X);
 document.body.addEventListener('keypress', (event) => {
 	if (event.key === ' ') {
 		dinoJumpStartTime = previousFrameTimestamp;
@@ -37,10 +35,29 @@ document.body.addEventListener('keypress', (event) => {
 
 // 60 FPS = 16.67ms between frames
 function renderScene(currentTimestamp = 0) {
-	frameCount += 1;
+	frameCount = (frameCount + 1) % Number.MAX_SAFE_INTEGER;
 
+	clearCanvas(gameCanvas);
+
+	// If msSinceLastFrame === 1000 (1s), how many pixels has the dino moved? => 100 (DINO_RUN_SPEED)
+	// Speed = Distance / time
+	// Speed * time = (Distance / time) * time   (time cancels on RHS)
+	// Speed * time = Distance
+	// Distance = Speed * time
+	const msSinceLastFrame = currentTimestamp - previousFrameTimestamp;
+	const secondsSinceLastFrame = msSinceLastFrame / 1000;
+	const distanceDinoHasMoved = DINO_RUN_SPEED * secondsSinceLastFrame;
+
+	drawPlayer(frameCount, currentTimestamp);
+	drawObstacles(distanceDinoHasMoved);
+
+	previousFrameTimestamp = currentTimestamp;
+	requestAnimationFrame(renderScene);
+}
+
+function drawPlayer(frameCount, currentTimestamp) {
 	// This if statement combines integer division with modular arithmetic to toggle
-	// the leg animation every dinoLegToggleFrameCount frames in a dynamic way.
+	// the leg animation every DINO_LEG_TOGGLE_FRAME_COUNT frames in a dynamic way.
 	// The % operator yields the integer remainder of division.
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
 	// num % 2 will yield the remainder of dividing a number by 2, so
@@ -50,38 +67,19 @@ function renderScene(currentTimestamp = 0) {
 	// Math.floor() performs integer division, so the decimal part of the quotient is thrown out.
 	// For example, 5/4 = 1.25, but Math.floor(5/4) = 1. Math.floor(9/4) = 2.
 	//
-	// So combining the two, the condition evaluates to 0 (false) the first dinoLegToggleFrameCount frames,
-	// then evaluates to 1 (true) the next dinoLegToggleFrameCount frames,
-	// then evaluates to 0 (false) the next dinoLegToggleFrameCount frames, and so on.
-	if (Math.floor(frameCount / dinoLegToggleFrameCount) % 2) {
-		leg_motion = legMotionTypes.rightLegUpLeftLegDown;
+	// So combining the two, the condition evaluates to 0 (false) the first DINO_LEG_TOGGLE_FRAME_COUNT frames,
+	// then evaluates to 1 (true) the next DINO_LEG_TOGGLE_FRAME_COUNT frames,
+	// then evaluates to 0 (false) the next DINO_LEG_TOGGLE_FRAME_COUNT frames, and so on.
+	let legMotion;
+	if (Math.floor(frameCount / DINO_LEG_TOGGLE_FRAME_COUNT) % 2) {
+		legMotion = legMotionTypes.rightLegUpLeftLegDown;
 	} else {
-		leg_motion = legMotionTypes.leftLegUpRightLegDown;
+		legMotion = legMotionTypes.leftLegUpRightLegDown;
 	}
 
-	clearCanvas(gameCanvas);
-
-	// If msSinceLastFrame === 1000 (1s), how many pixels has the dino moved? => 100 (dinoRunSpeed)
-	// Speed = Distance / time
-	// Speed * time = (Distance / time) * time   (time cancels on RHS)
-	// Speed * time = Distance
-	// Distance = Speed * time
-	const msSinceLastFrame = currentTimestamp - previousFrameTimestamp;
-	const secondsSinceLastFrame = msSinceLastFrame / 1000;
-	const distanceDinoHasMoved = dinoRunSpeed * secondsSinceLastFrame;
-	const newObjectXCoord = objStartingx - distanceDinoHasMoved;
-	objStartingx = newObjectXCoord;
-
-	console.log('Current time of animation frame', currentTimestamp);
-	console.log('Ms, s since last frame', msSinceLastFrame, secondsSinceLastFrame);
-	console.log('Dino moved distance, new x coord', distanceDinoHasMoved, newObjectXCoord);
-	console.log('************************************');
-
-	if (creatureType === creatureTypes.creature) {
-		// Test "Creature"
-		drawCreature(gameCanvas, startingx, startingy);
-	} else if (creatureType === creatureTypes.dino) {
-		// Attempt at Dino
+	if (CREATURE_TYPE === creatureTypes.creature) {
+		drawCreature(gameCanvas, PLAYER_X, PLAYER_Y);
+	} else if (CREATURE_TYPE === creatureTypes.dino) {
 		const dinoIsJumping = dinoJumpStartTime > 0;
 		let heightOfJump = 0;
 		if (dinoIsJumping) {
@@ -93,17 +91,47 @@ function renderScene(currentTimestamp = 0) {
 			}
 		}
 
-		drawDino(gameCanvas, startingx, startingy - heightOfJump, leg_motion);
+		drawDino(gameCanvas, PLAYER_X, PLAYER_Y - heightOfJump, legMotion);
+	}
+}
+
+function drawObstacles(distanceDinoHasMoved) {
+	for (const obstacleCoord of obstacleXCoords) {
+		const newObjectX = obstacleCoord.x - distanceDinoHasMoved;
+		obstacleCoord.x = newObjectX;
+
+		drawObstacle(newObjectX);
 	}
 
-	if (obstacleType === obstacleTypes.box) {
-		drawBox(gameCanvas, newObjectXCoord, objStartingy);
-	} else if (obstacleType === obstacleTypes.cactus) {
-		drawCactus(gameCanvas, newObjectXCoord, objStartingy);
-	}
+	obstacleXCoords = obstacleXCoords.filter(({x}) => !isObstacleOffscreen(x));
 
-	previousFrameTimestamp = currentTimestamp;
-	requestAnimationFrame(renderScene);
+	if (obstacleXCoords.length < 3) {
+		const lastObstacle = obstacleXCoords[obstacleXCoords.length - 1];
+		obstacleXCoords = [...obstacleXCoords, ...getRandomObstacles(lastObstacle.x)];
+	}
+}
+
+function isObstacleOffscreen(xCoord) {
+	return xCoord < -MIN_OBSTACLE_SPACING / 2;
+}
+
+function getRandomObstacleOffset() {
+	return Math.floor(Math.random() * INITIAL_OBSTACLE_X, MIN_OBSTACLE_SPACING);
+}
+
+function getRandomObstacles(objectXOffset) {
+	const obstacle1 = { x: objectXOffset + getRandomObstacleOffset() };
+	const obstacle2 = { x: obstacle1.x + getRandomObstacleOffset() };
+	const obstacle3 = { x: obstacle2.x + getRandomObstacleOffset() };
+	return [obstacle1, obstacle2, obstacle3];
+}
+
+function drawObstacle(xCoord) {
+	if (OBSTACLE_TYPE === obstacleTypes.box) {
+		drawBox(gameCanvas, xCoord, OBSTACLE_Y);
+	} else if (OBSTACLE_TYPE === obstacleTypes.cactus) {
+		drawCactus(gameCanvas, xCoord, OBSTACLE_Y);
+	}
 }
 
 renderScene();
